d2aa1394735fafc1d1cb618619093ac60dbf3e80
godedi <79082506+godedi@users.noreply.github.com>
2021-05-06 21:14:11 +0200

    speltest-updates

--- dog vs cake/doggostiff.gd
+++ dog vs cake/doggostiff.gd
@@ -8,9 +8,9 @@ var gravity = 28
 var jump_magnitude = 15
 
 var movement_speed = 0
-var walk_speed = 5
-var run_speed = 10
-var acceleration = 6
+var walk_speed = 11
+var run_speed = 19
+var acceleration = 8
 var angular_acceleration = 7 
 var push = 2
 
@@ -25,26 +25,19 @@ onready var hand = $doggo/hand
 
 onready var gun_a_hr = preload("res://gun A HR.tscn")
 onready var gun_a = preload("res://gun A.tscn")
-onready var gun_b_hr = preload("res://gun B HR.tscn")
-onready var gun_b = preload("res://gun B.tscn")
+
 
 #Koden för att kolla om du kan greppa saker
 func _process(delta):
 	if reach.is_colliding():
 		if reach.get_collider().get_name() == "gun A":
 			weapon_to_spawn = gun_a_hr.instance()
-		elif reach.get_collider().get_name() == "gun B":
-			weapon_to_spawn = gun_b_hr.instance()
 		else:
 			weapon_to_spawn = null
-	else:
-		weapon_to_spawn = null
 	
 	if hand.get_child(0) != null:
 		if hand.get_child(0).get_name() == "gun A HR":
 			weapon_to_drop = gun_a.instance()
-		elif hand.get_child(0).get_name() == "gun B HR":
-			weapon_to_drop = gun_b.instance()
 	else: 
 		weapon_to_drop = null
 		
@@ -55,12 +48,12 @@ func _process(delta):
 				get_parent().add_child(weapon_to_drop)
 				weapon_to_drop.global_transform = hand.global_transform
 				weapon_to_drop.dropped = true
-				hand.get_child(0).queue_free()
 			reach.get_collider().queue_free()
 			hand.add_child(weapon_to_spawn)
 			weapon_to_spawn.rotation = hand.rotation
 			
-		
+			
+			
 func _physics_process(delta):
 	
 	# Själva rörelsekoden. 
@@ -86,7 +79,7 @@ func _physics_process(delta):
 	
 	#Gör att hundens "Mesh" roterar. Så att den tittar åt det håll man går. 
 	$doggo.rotation.y = lerp_angle($doggo.rotation.y, atan2(-direction.z, direction.x), delta * angular_acceleration) 
-	#$head/Camera.rotation.y = lerp_angle($head/Camera.rotation.y, atan2(-direction.z, direction.x), delta * angular_acceleration) 
+	
 	#Gör så att hunden kan hoppa upp på en rigidbody
 	for index in get_slide_count():
 		var collision = get_slide_collision(index)
@@ -96,8 +89,6 @@ func _physics_process(delta):
 	#Fixar gravitation i spelet. 
 	if !is_on_floor():
 		vertical_velocity -= gravity * delta
-	#else:
-	#	vertical_velocity = 0
 		
 		#Mekanismen för att hoppa.
 	if is_on_floor():
@@ -105,4 +96,10 @@ func _physics_process(delta):
 			vertical_velocity = jump_magnitude
 		
 		
+	
+
+#Byter scen till the end när du nuddar tårtan
+func _on_Cake_body_entered(body):
+	if body.name == "dog":
+		get_tree().change_scene("res://TheEnd.tscn")
 	
