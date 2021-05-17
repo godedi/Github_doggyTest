503c768d2b691374473b754f16d24c4e252badaa
godedi <79082506+godedi@users.noreply.github.com>
2021-05-17 10:56:30 +0200

    Rättvänd hund

--- dog vs cake/button-title-play.gd
+++
@@ -1,8 +0,0 @@
-extends Button
-
-func _ready():
-	pass
-
-
-func _on_buttontitleplay_pressed():
-	get_tree().change_scene("res://Level.tscn")
---
+++ dog vs cake/scripts/gun A.gd
@@ -0,0 +1,14 @@
+extends RigidBody
+
+
+var dropped = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	add_to_group("bodies")
+	add_to_group("pickables")
+
+func _procces(delta):
+	if dropped == true:
+		apply_impulse(transform.basis.z, -transform.basis.z * 10)
+		dropped = false
