12c2c3d665a29457c104bb92be81ca04de820cda
godedi <79082506+godedi@users.noreply.github.com>
2021-06-02 14:02:53 +0200

    2

--- dog vs cake/CameraTarget1.gd
+++ dog vs cake/CameraTarget1.gd
@@ -11,10 +11,9 @@ func _ready():
 	pass # Replace with function body.
 
 
-func _physics_process(delta):
-	if global_transform.origin.z > 5:
-		global_transform.origin.z = get_parent().get_parent().global_transform.origin.z +1
-		#global_transform.origin.z = 5
-	else:
-		global_transform.origin.z = get_parent().get_parent().global_transform.origin.z +2
+#func _physics_process(delta):
+#	if global_transform.origin.z > 5:
+#		global_transform.origin.z = 5
+#	else:
+#		global_transform.origin.z = get_parent().get_parent().global_transform.origin.z +2
 
