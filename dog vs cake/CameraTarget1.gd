330f2ebd2ba59aeae2bc8292a733b133a8e5c555
godedi <79082506+godedi@users.noreply.github.com>
2021-06-02 13:52:22 +0200

    vygwuvbwbefb

---
+++ dog vs cake/CameraTarget1.gd
@@ -0,0 +1,20 @@
+extends Spatial
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _physics_process(delta):
+	if global_transform.origin.z > 5:
+		global_transform.origin.z = get_parent().get_parent().global_transform.origin.z +1
+		#global_transform.origin.z = 5
+	else:
+		global_transform.origin.z = get_parent().get_parent().global_transform.origin.z +2
+
--- dog vs cake/scripts/BlommorIkruka_hel.gd
+++ dog vs cake/scripts/BlommorIkruka_hel.gd
@@ -3,3 +3,8 @@ extends RigidBody
 
 func _ready():
 	add_to_group("bodies")
+
+
+func _on_BlomVas_body_entered(body):
+	if body.is_in_group("livingRoomFloor"):
+		get_node("destruction").destroy()
