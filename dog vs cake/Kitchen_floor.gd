4d484db4dbca3fd7f8d44463679b284461971daf
godedi <79082506+godedi@users.noreply.github.com>
2021-06-08 11:42:40 +0200

    it's the final countdown!

--- dog vs cake/Kitchen_floor.gd
+++ dog vs cake/Kitchen_floor.gd
@@ -2,6 +2,8 @@ extends StaticBody
 
 
 var glasSound = false
+var glasSound2 = false
+var glasSound3 = false
 
 
 func _ready():
@@ -16,3 +18,15 @@ func _on_glas_body_entered(body):
 	if body.is_in_group("kitchenFloor") and glasSound == false:
 		$GlasKras.play()
 		glasSound = true
+
+
+func _on_glas2_body_entered(body):
+	if body.is_in_group("kitchenFloor") and glasSound2 == false:
+		$GlasKras.play()
+		glasSound2 = true
+
+
+func _on_glas3_body_entered(body):
+	if body.is_in_group("kitchenFloor") and glasSound3 == false:
+		$GlasKras.play()
+		glasSound3 = true
